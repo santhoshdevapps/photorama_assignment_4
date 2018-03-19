eb8fbd3fd4f9b0d09e7a8f9691954dc9900cc9de
santhosh damodharan <santhoshdamodharan@santhoshs-macbook-air.local>
2018-03-19 19:48:41 -0400

    Added PhotoDataSource class & altered the PhotoViewController

---
+++ Photorama/PhotoDataSource.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoDataSource.swift
+//  Photorama
+//
+//  Created by santhosh damodharan on 2018-03-19.
+//  Copyright © 2018 santhosh damodharan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return  photos.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let identifier = "UICollectionViewCell"
+        
+        let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: <#T##IndexPath#>)
+        return cell
+    }
+    
+    
+    var photos = [Photo]()
+    
+}
